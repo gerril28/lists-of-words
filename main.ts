9e3b111309458a06a321e2b1a92019926f5431cc
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-08 09:26:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
-let list = [
-"a",
-"b",
-"c",
-"",
-"",
-"",
-"",
-"",
-"",
-""
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (list[0]))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (list[9]))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (list[3]))
+})
+let list: string[] = []
+list = [
+"Wonyoung",
+"Sakura",
+"Yuri",
+"Yena",
+"Yujin",
+"Nako",
+"Eunbi",
+"Hyewon",
+"Hitomi",
+"Chaewon"
 ]
